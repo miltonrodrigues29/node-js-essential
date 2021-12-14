afda1ddc2ad781b6b72829cbdc52d5f8688af333
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-12-14 11:40:43 +0530

    -

--- globalprocess.js
+++ globalprocess.js
@@ -3,5 +3,11 @@
 // console.log(process.argv)  //prints data associated with the process
 // console.log(`your name ${firstName} ${lastName}`);
 
+const grab = flag =>{
+    let indexAfterFlag = process.argv.indexOf(flag) +1;
+    return process.argv[indexAfterFlag];
+}
+
 const greeting = grab("--greeting");
 const user  = grab("--user");
+console.log(`${greeting} ${user}`);
