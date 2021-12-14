f6e6f199347a12b54e97e7d5d4fdf8c0f3c4cfa1
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-12-14 11:24:33 +0530

    -

---
+++ globalprocess.js
@@ -0,0 +1,7 @@
+// const [, , firstName,lastName] = process.argv
+
+// console.log(process.argv)  //prints data associated with the process
+// console.log(`your name ${firstName} ${lastName}`);
+
+const greeting = grab("--greeting");
+const user  = grab("--user");
