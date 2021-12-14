df028fc3fd27a8143d50c28925e9bc6e166a8155
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-12-14 11:21:05 +0530

    -

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const path = require("path")
+
+let  hello = "Hello World from node JS"
+
+global.console.log(hello)
+console.log(__dirname)
+console.log(__filename)
+console.log(`The file name is ${path.basename(__filename)}`)
+
+// console.log("Hello World");
+
+console.log(process.pid)
+console.log(process.version)
+console.log(process.argv)
